b98bc4e73a034cebf92a4e319bc89d8bf839966b
Nelli <nelli-asatryan@yandex.ru>
2021-08-21 22:57:28 +0300

    дз 2

--- Lesson_2/les2_2.py
+++ Lesson_2/les2_2.py
@@ -8,23 +8,3 @@ elements = input("Введите данные: ").split()
 for i in range(1, len(elements), 2):
     elements[i-1], elements[i] = elements[i], elements[i-1]
 print(elements)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
