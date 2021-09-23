72f466c016a089866abb23f843e3d5c60a9ff1d4
Nelli <nelli-asatryan@yandex.ru>
2021-09-23 14:33:36 +0300

    ДЗ 2

---
+++ lesson 2/les2_5.py
@@ -0,0 +1,15 @@
+rating = [7, 10, 4, 2, 9]
+print(f"rating = {rating}")
+rating.sort(reverse=True)
+print('Сортированный rating = ', rating)
+
+new_rating = int(input('Введите новое значение ретинга: '))
+
+for i in rating:
+    if new_rating > i and new_rating > 0:
+        rating.insert((rating.index(i)), new_rating)
+        print(rating)
+    elif new_rating < i:
+        rating.insert(len(rating), new_rating)
+        print(rating)
+        break
